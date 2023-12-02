306fc3876254ac9b9bf062409561ffd16a34a16f
tkachenya41 <tkachenya41@gmail.com>
2023-12-02 19:07:41 +0300

    feat: add more sustainable password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,18 +4,19 @@ import bcrypt from "bcrypt";
 const prisma = new PrismaClient();
 
 try {
-  const adminPassword = "12567";
+  const adminPassword = "12345romaN@#$";
   const hash = bcrypt.hashSync(adminPassword, 10);
   const admin = await prisma.user.create({
     data: {
       name: "ADMIN",
-      email: "admi@gmail.com",
+      email: "adminiu@gmail.com",
       password: hash,
       role: "ADMIN",
     },
   });
 } catch (e) {
   console.error(e);
+  console.log(e);
 } finally {
   await prisma.$disconnect();
   process.exit(1);
